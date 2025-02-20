cb40683036927f86c66b2f48c66204f866d62855
Korvised YOTSAIY <kvsyotsaiy@gmail.com>
2025-02-20 16:34:25 +0700

    reservation services

--- apps/reservations/src/dto/create-reservation.dto.ts
+++ apps/reservations/src/dto/create-reservation.dto.ts
@@ -1,6 +1,24 @@
+import { IsDate, IsDefined, IsNotEmpty, IsString } from 'class-validator';
+import { Type } from 'class-transformer';
+
 export class CreateReservationDto {
+  @IsDefined()
+  @IsDate()
+  @Type(() => Date)
   startDate: Date;
+
+  @IsDefined()
+  @IsDate()
+  @Type(() => Date)
   endDate: Date;
+
+  @IsDefined()
+  @IsString()
+  @IsNotEmpty()
   placeId: string;
+
+  @IsDefined()
+  @IsString()
+  @IsNotEmpty()
   invoiceId: string;
 }
